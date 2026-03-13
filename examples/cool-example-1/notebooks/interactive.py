407b2847a1865bcd1dfdbf1f2918a6a4dbb31607
Ray Hagimoto <rayhagimoto@gmail.com>
2026-03-13 13:06:28 -0500

    dvc: commit experiment 457dfa9298d2a543f73e546e62882e99d44b738b47855f53c81e949d7e67eeaf

--- examples/cool-example-1/notebooks/interactive.py
+++ examples/cool-example-1/notebooks/interactive.py
@@ -13,68 +13,82 @@ def _():
 
 @app.cell
 def _():
-    import dvc.api
-
-    return
-
-
-@app.cell
-def _():
-    import polars as pl
-
-    return (pl,)
-
-
-@app.cell
-def _(Literal, pl):
-    def exp_show(
-        hide_workspace=False,
-        hide_baseline=False,
-        return_type: Literal["dict", "df"] = "df",
-        **dvc_kwargs,
-    ):
-        import dvc.api
-
-        exps_dict = dvc.api.exp_show(**dvc_kwargs)
-        df = pl.DataFrame(exps_dict)
-
-        if hide_workspace:
-            df = df.filter(pl.col("rev") != "workspace")
-
-        if hide_baseline:
-            # this also hides workspace since 'workspace' commit always has 'typ'==baseline
-            df = df.filter(pl.col("typ") != "baseline")
-
-        if return_type == "dict":
-            df = df.to_dict()
-
-        return df
-
-    return (exp_show,)
-
-
-@app.cell
-def _(exp_show):
-    exps_df = exp_show(
-        hide_baseline=True, revs=["dvc_exp/cool-example-1/seed-sweep"]
-    )
-    exps_df
-    return
-
-
-@app.cell
-def _():
     import altair as alt
 
     return
 
 
-app._unparsable_cell(
-    r"""
-    def _plot_
-    """,
-    name="_"
-)
+@app.cell
+def _():
+    import matplotlib.pyplot as plt
+
+    return (plt,)
+
+
+@app.function
+def exp_show(
+    hide_workspace=False,
+    hide_baseline=False,
+    return_type: "dict" | "df" = "df",
+    **dvc_kwargs,
+):
+    import polars as pl
+    import dvc.api
+
+    exps_dict = dvc.api.exp_show(**dvc_kwargs)
+    df = pl.DataFrame(exps_dict)
+
+    if hide_workspace:
+        df = df.filter(pl.col("rev") != "workspace")
+
+    if hide_baseline:
+        # this also hides workspace since 'workspace' commit always has 'typ'==baseline
+        df = df.filter(pl.col("typ") != "baseline")
+
+    if return_type == "dict":
+        df = df.to_dict()
+
+    return df
+
+
+@app.cell
+def _():
+    _exps_df = exp_show(
+        hide_baseline=True, revs=["dvc_exp/cool-example-1/seed-sweep"]
+    )
+    _exps_df
+    return
+
+
+@app.cell
+def _():
+    exps_df = exp_show(hide_baseline=True)
+    return (exps_df,)
+
+
+@app.cell
+def _(exps_df, pl, plt):
+    def _make_plot(df: pl.DataFrame):
+        "Plot avg test precision vs max number of features"
+
+        df = df.sort("featurize.max_features")
+        fig, ax = plt.subplots(figsize=(4, 3))
+        ax.plot(df["featurize.max_features"], df["avg_prec.test"])
+        ax.set_xlabel("featurize.max_features")
+        ax.set_ylabel("avg_prec.test")
+
+        return fig
+
+
+    fig = _make_plot(exps_df)
+    fig
+    return
+
+
+@app.cell
+def _(exps_df):
+    exps_df
+    return
 
 
 if __name__ == "__main__":
